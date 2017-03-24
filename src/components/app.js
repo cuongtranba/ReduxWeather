4bd0ee0ef699190d8a76644edabe77a284ecb92f
CTRAN-DESKTOP\tranbacuong <ctran@amaris.com>
2017-03-24 11:52:13 +0700

    init

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Searchbar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Searchbar/>
+      </div>
     );
   }
 }
