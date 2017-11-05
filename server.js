ae01e68b83ccd563d2b24e8dd6a3f523789e1c4d
DESKTOP-LU0C24T\cuong <bacuongtr@gmail.com>
2017-11-05 21:53:59 +0700

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/'));
 //////////////////////
 app.get('*', (req, res) =>{
